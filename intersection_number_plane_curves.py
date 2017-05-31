d9b29f96fe84559e0f031ce76cb4b6ecf4fe78e3
dogestoyevsky <dogestoyevsky@gmail.com>
2017-05-30 22:18:07 -0400

    Works some of the time, but does not terminate other times.

--- intersection_number_plane_curves.py
+++ intersection_number_plane_curves.py
@@ -11,9 +11,9 @@ import numpy as np
 import math as math
 from IPython import embed
 
-# for now, assuming f(0) = 0, can write in translation step later
-inPolynomial1 = '3*x**2*y+x+y**4'
-inPolynomial2 = '6*x*y+x+8*y**4'
+# for now, assuming f(0,0) = 0, can write in translation step later
+inPolynomial1 = input('Type polynomial 1:')
+inPolynomial2 = input('Type polynomial 2:')
 
 x = Symbol('x')
 y = Symbol('y')
@@ -24,9 +24,13 @@ g = poly(inPolynomial2,x,y)
 
 sumOrder = 0
 
-while f(0,0) == 0 & g(0,0) == 0:
-    fPrime = poly(f(x,0),x)
-    gPrime = poly(g(x,0),x)
+while (f(0,0) == 0) & (g(0,0) == 0):
+    fPrime = sympify(f)
+    fPrime = fPrime.subs(y,0)
+    fPrime = poly(fPrime,x)
+    gPrime = sympify(g)
+    gPrime = gPrime.subs(y,0)
+    gPrime = poly(gPrime,x)
     
     if degree(fPrime) > degree(gPrime):
         hold_fPrime = fPrime
@@ -36,16 +40,31 @@ while f(0,0) == 0 & g(0,0) == 0:
         gPrime = hold_fPrime
         g = hold_f
       
-    while degree(fPrime) > 0
-        f = f/LC(fPrime)
-        g = g/LC(gPrime)
+    while degree(fPrime) > 0:
+        f = poly(f/LC(fPrime),x,y)
+        g = poly(g/LC(gPrime),x,y)
         degDifference = degree(gPrime) - degree(fPrime)
-        h = g - x**degDifference * f
-        hPrime = poly(f(x,0),x)
+        h = g - poly(x**degDifference,x,y) * f
+        hPrime = sympify(h)
+        hPrime = h.subs(y,0)
+        hPrime = poly(hPrime,x)
+
+        if degree(fPrime) > degree(hPrime):
+            hold_fPrime = fPrime
+            hold_f = f
+            fPrime = hPrime
+            f = h
+            hPrime = hold_fPrime
+            h = hold_f
+        else:
+            gPrime = hPrime
+            g = h
         
     sumOrder += degree(gPrime)
-    f = f/y
-        
+    f = poly(simplify(f/y),x,y)
+
+    
+print('The intersection number of your polynomials at 0 is: \n', sumOrder)
 
         
         
